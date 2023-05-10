6e945368b5b5e3ce65313ac1c4f21d8a9f4632e1
ankit jodhani <ankitjodhani1903@gmail.com>
2023-05-10 15:41:25 +0530

    done some configuration

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,19 @@ import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from "dotenv";
 const app = express();
+
+
+dotenv.config()
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan('common'));
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USERNAME,
+  password: process.env.PASSWORD,
   database: "test",
 });
 
